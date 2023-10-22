d67697b3469c4c1e1cbd107635a53b97f6cf00a7
dogcrunch <129897609+dogcrunch@users.noreply.github.com>
2023-10-21 21:39:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
 const defaultCode = 
-`// Example 1
-push 2
-push 3
+`// EXAMPLE - VARIABLES AND MATH
 push 5
-op +
-pow
-let x // the let keyword defines a variable as the top of stack
-
-push "x is equal 2^(3+5)"
-print
-
+push 6
+op * // the op keyword supports all js operators
+let x
 push "x = "
 push x
 op +
 print
 
-push "x + 5 = "
+
+
+// EXAMPLE - IF STATEMENT
 push x
-push 5
+push 4
+op >
+if // references bool at top of stack
+  push "x is greater than 4"
+  print
+end
+
+
+
+// EXAMPLE - FUNCTIONS
+function square x
+  push x
+  push x
+  op *
+  return
+end
+
+push "x squared is "
+push x
+square
 op +
-op +
-print`
+print
+
+
+
+// EXAMPLE - WHILE LOOP
+push 0
+let i
+
+while
+  push i
+  push 5
+  op <
+do
+  push i
+  print
+
+  push i
+  push 1
+  op +
+  let i
+end
+`
 
 document.getElementById("input").value = defaultCode;
 var editor = CodeMirror.fromTextArea(document.getElementById("input"), {
@@ -28,12 +63,19 @@ var editor = CodeMirror.fromTextArea(document.getElementById("input"), {
     theme: "dracula",
 
   });
+
   var output = CodeMirror.fromTextArea(document.getElementById("output"), {
     theme: "dracula",
 
   });
+  var stackOutput = CodeMirror.fromTextArea(document.getElementById("stack"), {
+    theme: "dracula",
+    lineWrapping: true,
+
+  });
   output.setSize("100%", "100%")
   editor.setSize("100%", "100%");
+  stackOutput.setSize("100%", "100%");
 
 document.getElementById("runButton").onclick = function(){
   parse(editor.getValue());
